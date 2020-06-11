5f1495cecdbccf0c8a864ac455ffcdce8ee7cd83
unknown <alex_s253@hotmail.com>
2020-06-11 20:07:01 +0930

    added more photos changed text created animals page started collect page

--- js/myscript.js
+++ js/myscript.js
@@ -36,6 +36,39 @@ function myFunction4() {
 
 }
 
+function myFunction5() {
+  var x = document.getElementById("hcontent5");
+  if (x.style.display === "none")
+    {x.style.display = "block";}
+   else {
+    x.style.display = "none";
+  }
+}
+function myFunction6() {
+  var x = document.getElementById("hcontent6");
+  if (x.style.display === "none")
+    {x.style.display = "block";}
+   else {
+    x.style.display = "none";
+  }
+}
+function myFunction7() {
+  var x = document.getElementById("hcontent7");
+  if (x.style.display === "none")
+    {x.style.display = "block";}
+   else {
+    x.style.display = "none";
+  }
+}
+function myFunction8() {
+  var x = document.getElementById("hcontent8");
+  if (x.style.display === "none")
+    {x.style.display = "block";}
+   else {
+    x.style.display = "none";
+  }
+}
+
 function navFunc() {
   var x = document.getElementById("nav_menu");
   if (x.style.display === "none")
@@ -61,4 +94,19 @@ function scrollFunction() {
 function topFunction() {
   d.body.scrollTop = 0;
   d.documentElement.scrollTop = 0;
+}
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
