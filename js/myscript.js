0227166fdde9dfa36708c5d865be97acfdcffbca
unknown <alex_s253@hotmail.com>
2020-07-06 14:38:08 +0930

    fixed hamburger menu for mobile fixed card sizing on mobile fixed hamburger js

--- js/myscript.js
+++ js/myscript.js
@@ -71,9 +71,9 @@ function myFunction8() {
 
 function navFunc() {
   var x = document.getElementById("nav_menu");
-  if (x.style.display === "none")
-    {x.style.display = "block";}
-   else {
+  if (x.style.display === "block")
     {x.style.display = "none";}
+   else {
+    {x.style.display = "block";}
   }
 }
