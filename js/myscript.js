6a87df94e29de5b739c14182280d0bc685900ef3
unknown <alex_s253@hotmail.com>
2020-07-12 15:35:40 +0930

    deleted unncessary files created year 3 interactive graph for activity 2

--- js/myscript.js
+++ js/myscript.js
@@ -77,3 +77,175 @@ function navFunc() {
     {x.style.display = "block";}
   }
 }
+
+
+//increment counter funcs dead trees
+function incrementValueRabbitrat()
+{
+    var value = parseInt(document.getElementById('rabbitratNumber').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('rabbitratNumber').value = value;
+}
+
+function incrementValueBlackrat()
+{
+    var value = parseInt(document.getElementById('blackratNumber').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('blackratNumber').value = value;
+}
+
+function incrementValueLizard()
+{
+    var value = parseInt(document.getElementById('lizardNumber').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('lizardNumber').value = value;
+}
+
+function incrementValueGlider()
+{
+    var value = parseInt(document.getElementById('gliderNumber').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('gliderNumber').value = value;
+}
+
+//create icon for dead tree graph
+function blackRat() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/blackrat.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("blackrat");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function rabbitRat() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/rabbitrat.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("rabbitrat");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function lizard() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/goanna.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("lizard");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function glider() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/glider.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("glider1");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+//create icon for live tree graph
+function blackRat2() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/blackrat.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("blackrat2");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function rabbitRat2() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/rabbitrat.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("rabbitrat2");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function lizard2() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/goanna.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("lizard2");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function glider2() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/glider.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("glider3");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function possum() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/possum.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("possum");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+//create icon for log graph
+function blackRat3() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/blackrat.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("blackrat3");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function rabbitRat3() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/rabbitrat.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("rabbitrat3");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function lizard3() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/goanna.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("lizard3");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function python() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/python.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("python");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function dingo() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/dingo.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("dingo");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
+
+function bandicoot() {
+  var pic = document.createElement("img");
+  pic.src = '../images/animal_imgs/bandicoot.jpg';
+  pic.className= 'rounded2';
+  var test = document.getElementById("bandicoot");
+  pic.innerHTML = "test yo";
+  test.appendChild(pic);
+}
